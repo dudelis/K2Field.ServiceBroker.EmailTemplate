d0a6c8d6eaee45be8815a5362a674ea79bf6971a
Konstantin Fukszon <fukszon.kostya@gmail.com>
2017-11-09 16:07:15 -0800

    Added Service Configuration class with constants for Config Section

---
+++ K2Field.ServiceBroker.EmailTemplate/K2Field.ServiceBroker.EmailTemplate/Constants/ServiceConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2Field.ServiceBroker.EmailTemplate.Constants
+{
+    public static class ServiceConfiguration
+    {
+        public const string ServerName = "ServerName";
+        public const string Port = "Port";
+        public const string DelimitedInputIDs = "DelimitedInputIDs";
+    }
+}
