533bfe1cca6fe584d14e1a97597983faa4388360
Konstantin Fukszon <fukszon.kostya@gmail.com>
2017-11-16 14:47:04 -0800

    #2 Added basic installer, which copies the files and registers the service type in K2.

---
+++ K2Field.ServiceBroker.EmailTemplate/Installer/Install.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module "$PSScriptRoot\K2HelperModule.psm1" -Force
+
+$guid = [guid]"99b22dd4-50a1-4ab4-aa91-80819e953a05" 
+$brokerName = "K2Field.ServiceBroker.EmailTemplate"
+$brokerSystemName = $brokerName
+$brokerDLL = "K2Field.ServiceBroker.EmailTemplate.dll"
+$displayName = $brokerName -replace '\.','_'
+$brokerDescription = "The Email Template Service Broker. For more information, see https://github.com/dudelis/K2Field.ServiceBroker.EmailTemplate"
+$k2ServerName = "localhost"
+$port = 5555
+$targetPath = GetK2InstallPath -machine $k2ServerName
+
+StopK2Service
+Write-Host "Copying $brokerDLL to the ServiceBroker folder"
+Copy-Item "$PSScriptRoot\$brokerDLL" -Destination "$targetPath\ServiceBroker"
+StartK2Service
+
+Write-Host "Registering Service Type with name $displayName"
+RegisterServiceType -ServiceTypeDLL $brokerDLL -guid $guid -systemName $brokerSystemName -displayName $displayName -description $brokerDescription -k2server $k2ServerName -port $port
---
+++ K2Field.ServiceBroker.EmailTemplate/Installer/K2HelperModule.psm1
@@ -0,0 +1,98 @@
+# Starts K2 Blackpearl Server Service
+# Added new name for the K2 Service, valid for K2 Five
+Function StartK2Service() {
+	Write-Host  "Starting K2 blackpearl service"
+	$job = Start-Job -ScriptBlock {
+		Get-Service | where-object {($_.DisplayName -eq "K2 blackpearl Server" -or $_.DisplayName -eq "K2 Server") -and $_.Status -ne "Running"} | Start-Service
+	}
+	Wait-Job $job
+	Receive-Job $job
+}
+
+# Stops K2 Blackpearl Server Service
+# Added new name for the K2 Service, valid for K2 Five
+Function StopK2Service() {
+    Write-Host "Stopping K2 blackpearl service"
+
+	$job = Start-Job -ScriptBlock {
+		Get-Service | where-object {($_.DisplayName -eq "K2 blackpearl Server" -or $_.DisplayName -eq "K2 Server") -and $_.Status -eq "Running"} | Stop-Service -Confirm:$false -Force 
+		Stop-Process -ProcessName "K2HostServer"  -force -ErrorAction SilentlyContinue -Confirm:$false
+	}
+	Wait-Job $job
+	Receive-Job $job
+}
+
+Function GetK2InstallPath([string]$machine = $env:computername) {
+    $registryKeyLocation = "SOFTWARE\SourceCode\BlackPearl\blackpearl Core\"
+    $registryKeyName = "InstallDir"
+
+	Write-Debug "Getting K2 install path from $machine "
+    
+    $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $machine)
+    $regKey= $reg.OpenSubKey($registryKeyLocation)
+    $installDir = $regKey.GetValue($registryKeyName)
+    return $installDir
+}
+# Returns the version number of the installer of K2
+Function GetK2VersionNumber([string]$machine = $env:computername) {
+    $registryKeyLocation = "SOFTWARE\SourceCode\Installer\"
+    $registryKeyName = "Version"
+
+	Write-Debug "Getting the version of the K2 Installer from $machine "
+    
+    $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $machine)
+    $regKey= $reg.OpenSubKey($registryKeyLocation)
+    $installDir = $regKey.GetValue($registryKeyName)
+    return $installDir
+}
+
+Function GetK2ConnectionString([string]$k2Server = "localhost", [int]$port = 5555) {
+    Write-Debug "Creating connectionstring for machine '$k2Server' and port '$port'";
+	$hostClientApiPath = "$PSScriptRoot\Assemblies\SourceCode.HostClientAPI.dll"
+	[Reflection.Assembly]::LoadFile($hostClientApiPath) | Out-Null
+		
+    $connString = New-Object -TypeName "SourceCode.Hosting.Client.BaseAPI.SCConnectionStringBuilder";
+    $connString.Host = $k2Server;
+    $connString.Port = $port;
+	$connString.Integrated = $true;
+    $connString.IsPrimaryLogin = $true;
+	$connString.CachePassword = $false;
+	
+	return $connString.ConnectionString;    
+}
+
+Function RegisterServiceType([string]$ServiceTypeDLL, [guid]$guid, [string]$systemName, [string]$displayName, [string]$description = "", [string]$k2Server = "localhost", [int]$port = 5555) {
+    # Get Paths for local environment and for the remote machine, we might run this installer from a simple windows 7 host, while we deploy to a server that has a different drive...
+    $k2Path = GetK2InstallPath
+    $remK2Path = GetK2InstallPath -machine $k2Server
+    $smoManServiceAssembly = "$PSScriptRoot\Assemblies\SourceCode.SmartObjects.Services.Management.dll"
+    $serviceBrokerAssembly = $remK2Path + "ServiceBroker\$ServiceTypeDLL"
+	$className = "K2Field.ServiceBroker.EmailTemplate.EmailTemplateServiceBroker";
+	
+    Write-Debug "Adding/Updating ServiceType $serviceBrokerAssembly with guid $guid"
+	Write-Debug  "ServiceBrokerAssembly: $serviceBrokerAssembly"
+    
+    [Reflection.Assembly]::LoadFile($smoManServiceAssembly) | Out-Null
+    $smoManService = New-Object SourceCode.SmartObjects.Services.Management.ServiceManagementServer
+
+    #Create connection and capture output (methods return a bool)
+    $tmpOut = $smoManService.CreateConnection()
+	$k2ConnectionString = GetK2ConnectionString -k2Server $k2Server -port $port;
+    $tmpOut = $smoManService.Connection.Open($k2ConnectionString);
+    Write-Debug "Connected to K2 host server"
+
+    # Check if we need to update or register a new one...
+    if ([string]::IsNullOrEmpty($smoManService.GetServiceType($guid)) ) {
+        Write-Debug "Registering new service type..."
+        $tmpOut = $smoManService.RegisterServiceType($guid, $systemName, $displayName, $description, $serviceBrokerAssembly, $className);
+        write-debug "Registered new service type..."
+    } else {
+        Write-Debug "Updating service type..."
+        $tmpOut = $smoManService.UpdateServiceType($guid, $systemName, $displayName, $description, $serviceBrokerAssembly, $className);
+        Write-Debug "Updated service type..."
+    }
+    $smoManService.Connection.Close();
+    write-host "Deployed service-type"
+}
+
+Export-ModuleMember -Function StartK2Service, StopK2Service, GetK2InstallPath, RegisterServiceType, GetK2VersionNumber
